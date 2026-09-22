c37d81796493ae6e5780dab86f07a3d3b3a39bc8
mlkinteh2 <kintehmodoulamin02@gmail.com>
2026-09-23 04:15:20 +0800

    Pin Vercel Python version for Django

--- api/index.py
+++ api/index.py
@@ -4,4 +4,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medpredict.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
